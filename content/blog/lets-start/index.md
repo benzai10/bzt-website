71c3400e69f6838db376080de37c12649ed8f494
Oojooman <oojooman@gmail.com>
2020-02-16 19:09:51 +0100

    add aboutme page and edit first blog article

--- content/blog/lets-start/index.md
+++
@@ -1,7 +0,0 @@
----
-title: Let's Start!
-date: "2020-02-16T14:39:03.284Z"
-description: "Everything starts somewhere"
----
-
-Setup a blog in 30 minutes with Gatsby. Very cool.
---
+++ pages/aboutme.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => {
+  return (
+    <div>
+      <h1>About me - Hello</h1>
+    </div>
+  )
+}
